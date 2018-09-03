a452cb107739c6c096939dcf48e140bada872e5e
IBSOLUTION\D530 <mahendra.gulla@ibsolution.de>
2018-09-03 23:53:30 +0200

    Express Serve Static Files

--- 3_node_express/index.js
+++ 3_node_express/index.js
@@ -1,10 +1,12 @@
 const express = require("express"),
       http = require("http"),
+      morgan = require("morgan"),
       hostname = "localhost",
       port = 3001;
 
 const app = express();
-
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
 app.use((req, res, next) => {
     console.log(req.headers);
     res.statusCode = 200;
@@ -12,6 +14,7 @@ app.use((req, res, next) => {
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 
 });
+
 const server = http.createServer(app);
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
