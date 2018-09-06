bc24d6ea1e034eaae13cc37bf8680535d5623eea
IBSOLUTION\D530 <mahendra.gulla@ibsolution.de>
2018-09-06 19:05:10 +0200

    Node MongoDB example2

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,13 +1,37 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
-
+const dboper = require("./operations");
 const url = 'mongodb://localhost:27017/conFusion';
 const dbname = 'conFusion';
 MongoClient.connect(url,{ useNewUrlParser: true }, (err, client) => {
     assert.equal(err, null);
     console.log("Connected correctly to the Server");
-    const db = client.db(dbname),
-        collection = db.collection("dishes");
+    const db = client.db(dbname);
+    dboper.insertDocument(db, {name:"Vadonut", description:"Test"}, "dishes", (result)=>{
+        console.log("Insert Document\n", result.ops);
+        dboper.findDocuments(db, "dishes", (docs) => {
+            console.log("Found Documents:\n", docs);
+            dboper.updateDocument(db,{name:"Vadonut"}, {description:"Update test"}, "dishes", (result)=>{
+                console.log("Update documents\n", result.result);
+                dboper.findDocuments(db, "dishes", (docs)=>{
+                    console.log("Found updated documents\n", docs);
+                    db.dropCollection("dishes", (result)=>{
+                        console.log("Dropped collection", result)
+                        client.close();
+                    })
+                })
+
+            })
+
+
+        })
+    } )
+
+
+
+
+    
+     /*    collection = db.collection("dishes");
 
     collection.insertOne({ "name": "Upma", "description": "made of wheat grail" },
         (err, result) => {
@@ -30,6 +54,6 @@ MongoClient.connect(url,{ useNewUrlParser: true }, (err, client) => {
 
 
         }
-    )
+    ) */
 }
 );
