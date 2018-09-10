9be704da3a15e6ffda99fc5f5c9848d1a3d0072e
IBSOLUTION\D530 <mahendra.gulla@ibsolution.de>
2018-09-10 14:59:57 +0200

    passport

--- 4_express-generator/conFusionServer/app.js
+++ 4_express-generator/conFusionServer/app.js
@@ -1,25 +1,29 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var index = require('./routes/index');
+var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
-const mongoose = require("mongoose"),
-  session = require("express-session"),
-  FileStore = require("session-file-store")(session),
-  Dishes = require("./models/dishes"),
-  url = 'mongodb://localhost:27017/conFusion',
-  connect = mongoose.connect(url);
 
-connect.then((db) => {
-  console.log("Connected correctly to the server");
-}, (err) => { console.log(err) })
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+// Connection URL
+connect.then((client) => {
+    console.log('Connected correctly to server');
+});
 
 var app = express();
 
@@ -27,107 +31,57 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser('12345-31016-94100-75012'));
 app.use(session({
-  name: "session-id",
-  secret: "12345-67890-09876-54321",
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
-app.use('/', indexRouter);
+    name: 'session-id',
+    secret: '12345-31016-94100-75012',
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore()
+  }));
 
-app.use("/users", usersRouter);
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.use('/', index);
+app.use('/users', users);  
 
-function auth(req, res, next) {
-  console.log(req.session);
+function auth (req, res, next) {
+    console.log(req.user);
 
-  if (!req.session.user) {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    return next(err);
-  }
-  else {
-    if (req.session.user === 'authenticated') {
-      next();
+    if (!req.user) {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');                          
+      err.status = 401;
+      next(err);
     }
     else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
+          next();
     }
-  }
 }
-
-
-/* function auth (req, res, next) {
-  console.log(req.session);
-if(!req.session.user){
-
-  var authHeader = req.headers.authorization;
-  if (!authHeader) {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-      return;
-  }
-
-  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-  var user = auth[0];
-  var pass = auth[1];
-  if (user == 'admin' && pass == 'password') {
-    req.session.user = 'admin';
-    //res.cookie('user', 'admin', {signed:true});
-
-      next(); // authorized
-  } else {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');      
-      err.status = 401;
-      next(err);
-  }
-
-}
-else{
-
-  if(req.session.user === 'admin'){
-    console.log("req.session", req.session)
-    next();
-  }
-  else{
-    var err = new Error('You are not authenticated!');
-    err.status = 401;
-    next(err);
-
-  }
-
-
-}
-
-} */
-
 
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/dishes', dishRouter);
-app.use('/promotions', promoRouter);
-app.use('/leaders', leaderRouter);
+
+app.use('/dishes',dishRouter);
+app.use('/promotions',promoRouter);
+app.use('/leaders',leaderRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
